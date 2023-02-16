68e89839dfd228e9f0022d74781be34577c7a120
ali-programmer-1999 <ali.programer.1999@gmail.com>
2023-02-16 00:20:57 -0800

    finish weather webApp

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import { BsFillEmojiSunglassesFill, BsSearch } from 'react-icons/bs'
 import axios from 'axios'
 import Popup from './Popup'
+import Cart from './Cart'
 
 
 
@@ -22,7 +23,6 @@ export default function Home() {
       setShow(true)
     }
   }
-
   return (
     <>
       <Head>
@@ -32,37 +32,34 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        <section className='w-full h-full flex flex-col items-center py-7 bg-[#0000004f] relative'>
+        <section className='w-full h-full flex flex-col items-center py-10 bg-[#0000004f] relative'>
           {/* logo website  */}
 
           <h1 className={styles.logo}><BsFillEmojiSunglassesFill className='mb-2' /> MR.WatherApp</h1>
 
           {/* search location  form  */}
           <form className='w-full' onSubmit={submitHandler}>
-            <div className='w-[70%] mx-auto relative'>
-              <input type="text" required placeholder='Search your location' value={input} onChange={(e) => setInput(e.target.value)} className='p-3 px-8 w-full rounded-full font-bold' />
-              <button className='absolute flex justify-center items-center right-3 top-[50%] translate-y-[-50%] bg-gray-300 transition-all hover:bg-gray-600 hover:text-white p-2 rounded-full px-7'>
+            <div className='w-[80%] lg:w-[60%] mx-auto relative'>
+              <input type="text" required placeholder='Search your location' value={input} onChange={(e) => setInput(e.target.value)} className='p-4 px-8 w-full rounded-full font-bold text-md' />
+              <button type='submit' className='absolute flex justify-center items-center right-3 top-[50%] translate-y-[-50%] bg-gray-300 transition-all hover:bg-gray-600 hover:text-white p-3 rounded-full px-9'>
                 <BsSearch className='text-xl' />
               </button>
             </div>
           </form>
-          <section className='w-2/3 mt-5 h-full'>
-            {Data == undefined ? null : }
-            {show ? <Popup close={() => setShow(false)} /> : null}
-          </section>
+
+          {Data == null ?
+            <div className='h-1/3 flex justify-center items-center'>
+              <h1 className='text-3xl text-white'>Plase search your Location</h1>
+            </div>
+            :
+            <section className='w-[80%] lg:w-[55%] mt-10 h-full'>
+              {Data == undefined ? null : <Cart data={Data} />}
+              {show ? <Popup close={() => setShow(false)} /> : null}
+            </section>
+          }
         </section>
 
       </main>
     </>
   )
-}
-
-export async function getServerSideProps(ctx) {
-
-
-  return {
-    props: {
-      data: null
-    }
-  }
 }
